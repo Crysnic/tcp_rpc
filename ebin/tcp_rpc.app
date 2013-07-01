d28a6f9351b77d53b624330665a6f64a35bb60f7
Volodymyr <volodymyr@ubuntu-PC.(none)>
2013-07-01 20:29:25 +0300

    Troubleshouting

--- ebin/tcp_rpc.app
+++ ebin/tcp_rpc.app
@@ -2,7 +2,7 @@
              [{description,"RPC server for Erlang and OTP in action"},
               {vsn,"0.1.0"},
               {registered,[tcp_rpc_sup]},
-              {modules,[tcp_rpc_app,tcp_rpc_server,tcp_rpc_sup]},
               {applications,[kernel,stdlib]},
               {mod,{tcp_rpc_app,[]}},
-              {env,[]}]}.
+              {env,[]},
+              {modules,[tcp_rpc_app,tcp_rpc_server,tcp_rpc_sup]}]}.
--- src/tcp_rpc_server.erl
+++ src/tcp_rpc_server.erl
@@ -25,7 +25,6 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
 		 terminate/2, code_change/3]).
 
--define(SERVER, ?MODULE).
 -define(DEFAULT_PORT, 1055).
 
 -record(state, {port, lsock, request_count = 0}).
@@ -36,37 +35,29 @@
 
 %%--------------------------------------------------------------------------
 %% @doc Starts the srever.
-%%
-%% @spec start_link(Port::integer()) -> {ok, Pid}
-%% where
-%% Pid = pid()
-%% @end
 %%--------------------------------------------------------------------------
+-spec start_link(Port::integer()) -> {ok, pid()}.
 start_link(Port) ->
-	gen_server:start_link({local, ?SERVER}, ?MODULE, [Port], []).
+	gen_server:start_link({local, ?MODULE}, ?MODULE, [Port], []).
 
-%% @spec start_link() -> {ok, Pid}
 %% @doc Calls `start_link(Port)' using the default port.
+-spec start_link() -> {ok, pid()}.
 start_link() ->
 	tcp_rpc_server:start_link(?DEFAULT_PORT).
 
 %%--------------------------------------------------------------------------
 %% @doc Fetches the number of requests made to this server.
-%% @spec get_count() -> {ok, Count}
-%% where
-%%  Count = integer()
-%% @end
 %%--------------------------------------------------------------------------
+-spec get_count() -> {ok, Count::integer()}.
 get_count() ->
-	gen_server:call(?SERVER, get_count).
+	gen_server:call(?MODULE, get_count).
 
 %%--------------------------------------------------------------------------
 %% @doc Stops the server.
-%% @spec stop() -> ok
-%% @end
 %%--------------------------------------------------------------------------
+-spec stop() -> ok.
 stop() ->
-	gen_server:cast(?SERVER, stop).
+	gen_server:cast(?MODULE, stop).
 
 %%%=========================================================================
 %%% gen_server callbacks
